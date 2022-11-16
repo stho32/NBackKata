fdc76de76ce03ac9553a87a590bb3aa6b6e37844
Stefan Hoffmann <stho32github@gmail.com>
2022-11-16 09:46:20 +0100

    Everything from the RunTestInteractor.cs has now its own folder, so it does not get mixed up with other things.

---
+++ Source/nback/nback.Interactors/RunTest/RunTestDomainModel.cs
@@ -0,0 +1,8 @@
+﻿namespace nback.Interactors;
+
+public record RunTestDomainModel(
+    bool YouHaveReachedTheEnd,
+    string Stimulus,
+    bool DeclarePatternARepetitionIsLoggedIn,
+    bool DeclarePatternIsNotARepetitonIsLoggedIn,
+    string Message);
--- Source/nback/nback.Interactors/RunTest/RunTestInteractor.cs
+++ Source/nback/nback.Interactors/RunTest/RunTestInteractor.cs
@@ -4,14 +4,6 @@ using nback.Domain.Timers;
 
 namespace nback.Interactors;
 
-public record RunTestDomainModel(
-    bool YouHaveReachedTheEnd,
-    string Stimulus,
-    bool DeclarePatternARepetitionIsLoggedIn,
-    bool DeclarePatternIsNotARepetitonIsLoggedIn,
-    string Message);
-
-
 public class RunTestInteractor
 {
     private readonly ConfigurationForTheTest _configurationForTheTest;
